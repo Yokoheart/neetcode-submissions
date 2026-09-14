bda4cff65408d9ebb42620c8d8f56c6cca053f48
Yokoheart <122078548+Yokoheart@users.noreply.github.com>
2026-09-14 10:13:02 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.ts
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        if(s.length !== t.length){
+            return false;
+        }
+        const countS : Record<string, number> = {}; 
+        const countT : Record<string, number> = {};
+
+        for(let i = 0; i < s.length; i++){
+            countS[s[i]] = 1 + (countS[s[i]] || 0);
+            countT[t[i]] = 1 + (countT[t[i]] || 0);
+        }
+
+        for(let c in countS){
+            if(countS[c] !== (countT[c] || 0)){
+                return false;
+            }
+        } 
+        return true;
+    }
+}
